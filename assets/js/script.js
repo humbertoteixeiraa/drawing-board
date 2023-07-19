bdb9b64ec06ef54deae60fadfe2d0e099b658929
Humberto Teixeira <humberto.quim@gmail.com>
2023-07-19 17:06:26 -0300

    Updates of the day 07.19.2023

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// INITIAL DATA
+let currentColor = 'black';
+let screen = document.querySelector('#tela');
+let context = screen.getContext('2d');
+
+// EVENTS
+document.querySelectorAll('.colorArea .color').forEach(item => {
+    item.addEventListener('click', colorClickEvent);
+});
+
+// FUNCTIONS
+function colorClickEvent(e) {
+    let color = e.target.getAttribute('data-color');
+    currentColor = color;
+
+    document.querySelector('.color.active').classList.remove('active');
+    e.target.classList.add('active');
+}
