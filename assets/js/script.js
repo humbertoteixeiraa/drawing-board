01e962822f8aed072a58f72c3147a3edea068d12
Humberto Teixeira <humberto.quim@gmail.com>
2023-07-20 16:31:27 -0300

    Updates of the day 07.20.2023

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,25 @@
 // INITIAL DATA
 let currentColor = 'black';
+let canDraw = false;
 let screen = document.querySelector('#tela');
 let context = screen.getContext('2d');
+let mouseX = 0;
+let mouseY = 0;
 
 // EVENTS
 document.querySelectorAll('.colorArea .color').forEach(item => {
     item.addEventListener('click', colorClickEvent);
 });
+
+/* 
+ Drawing logic steps:
+ - When mouse click is pressed, drawing mode will be activated;
+ - When the mouse moves, and drawing mode is activated, drawing will start;
+ - When the mouse click is released, drawing mode will be disabled.
+*/
+screen.addEventListener('mousedown', mouseDownEvent);
+screen.addEventListener('mousemove', mouseMoveEvent);
+screen.addEventListener('mouseup', mouseUpEvent);
 
 // FUNCTIONS
 function colorClickEvent(e) {
@@ -15,4 +28,39 @@ function colorClickEvent(e) {
 
     document.querySelector('.color.active').classList.remove('active');
     e.target.classList.add('active');
+}
+
+function mouseDownEvent(e) {
+    canDraw = true;
+    mouseX = e.pageX - screen.offsetLeft;
+    mouseY = e.pageY - screen.offsetTop;
+}
+
+function mouseMoveEvent(e) {
+    if(canDraw === true) {
+        draw(e.pageX, e.pageY);
+    }
+
+}
+
+function mouseUpEvent() {
+    canDraw = false;
+}
+
+function draw(x, y) {
+    let pointX = x - screen.offsetLeft;
+    let pointY = y - screen.offsetTop;
+
+    /* Drawing */
+    context.beginPath();
+    context.lineWidth = 5;
+    context.lineJoin = 'round';
+    context.moveTo(mouseX, mouseY);
+    context.lineTo(pointX, pointY);
+    context.closePath();
+    context.strokeStyle = currentColor;
+    context.stroke();
+
+    mouseX = pointX;
+    mouseY = pointY;
 }
